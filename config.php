078eeeeb5fbae85b5e41070c43ece059c392cdec
heliodoros <heliodoros1992@gmail.com>
2021-02-05 19:16:51 +0200

    Update config.php

--- config.php
+++ config.php
@@ -9,6 +9,6 @@
     // define global constants
 
 	define ('ROOT_PATH', realpath(dirname(__FILE__)));
-	define ('BASE_URL', 'http://localhost/blog/');
+	define ('BASE_URL', 'http://localhost/');
 	define ('PAGE_NAME', 'Change page name');
 ?>
